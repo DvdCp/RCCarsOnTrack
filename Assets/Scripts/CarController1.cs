446a5cba795f8b5aaa61b2a4752f10de9c6ccbfe
DvdCp <davidecap00@hotmail.it>
2021-11-01 21:50:37 +0100

    Park stuff added

---
+++ Assets/Scripts/CarController1.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class CarController1 : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private float horizontalInput;
+    private float verticalInput;
+    private bool isBraking;
+    private float currentbrakeForce;
+    private float currentSteerAngle;
+    
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
+    [SerializeField] private float brakeForce;
+    [SerializeField] private float motorForce;
+    [SerializeField] private float maxSteeringAngle;
+
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawLine(transform.position + transform.up, transform.position - (transform.up * 2));
+    //}
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis(HORIZONTAL);
+        verticalInput = Input.GetAxis(VERTICAL);
+        isBraking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
+    {
+        //frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        //frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        rearLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        rearRightWheelCollider.motorTorque = verticalInput * motorForce;
+
+        currentbrakeForce = isBraking ? brakeForce : 0f;
+
+        ApplyBrakingForce();
+
+    }
+
+    private void ApplyBrakingForce()
+    {
+        frontLeftWheelCollider.brakeTorque = currentbrakeForce;
+        frontRightWheelCollider.brakeTorque = currentbrakeForce;
+        rearLeftWheelCollider.brakeTorque = currentbrakeForce;
+        rearRightWheelCollider.brakeTorque = currentbrakeForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteeringAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
+}
